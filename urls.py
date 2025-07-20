5e04d13569de97b2dfd94bf13f70486a469f7760
Nikita Novikov <n.n.s84@mail.ru>
2025-07-20 16:55:08 +0300

    Проект project_7

---
+++ urls.py
@@ -0,0 +1,4 @@
+base_url = 'https://qa-scooter.praktikum-services.ru '
+url_create_courier = f"{base_url}/api/v1/courier"
+url_courier_login = f"{base_url}/api/v1/courier/login"
+url_create_order = f"{base_url}/api/v1/orders"
